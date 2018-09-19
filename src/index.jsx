ec796f3757f2559a0c86a1111796a15f82cfaacc
flyerH <hzw758@qq.com>
2018-09-19 23:41:18 +0800

    run

--- src/index.jsx
+++ src/index.jsx
@@ -8,25 +8,29 @@ class ReactCanvasNest extends Component {
         const { style, config } = this.props;
 
         const canvasStyle = {
-            zIndex: -1,
-            opacity: 0.5,
-            color: '0,0,0',
-            display: 'block',
+            zIndex      : -1,
+            opacity     : 1,           // transparency of canvas     
+            display     : 'block',
             ...style
         }
 
         const canvasConfig = {
-            count: 99,
-            dist: 6000,
+            count: 99,     // count of points 
+            dist : 6000,   // maximum length of line segments between two points
+            pointOpacity: 1,           // transparency of points
+            lineColor   : '0,0,0',
+            lineWidth   :  10,           // multiple of line width
+            pointColor  : '255,0,0',
+            pointR      : 3,// radius of the point
             ...config,
         };
 
         this.state = {
-            outDivWidth: null,
+            canvasRef   : null,
+            outDivWidth : null,
             outDivHeight: null,
             canvasStyle,
-            canvasConfig,
-            canvasRef: null
+            canvasConfig
         }
 
     }
@@ -35,7 +39,7 @@ class ReactCanvasNest extends Component {
         const { canvasRef } = this;
 
         this.setState({
-            outDivWidth: canvasRef.parentNode.clientWidth,
+            outDivWidth : canvasRef.parentNode.clientWidth,
             outDivHeight: canvasRef.parentNode.clientHeight
         }, this.init);
     }
@@ -45,14 +49,16 @@ class ReactCanvasNest extends Component {
         const { canvasConfig } = this.state;
         // const canvasRef = this.canvasRef.current;
         const { canvasRef } = this;
-        const points = [];
+        const points        = [];
+        const width         = canvasRef.clientWidth;
+        const height        = canvasRef.clientHeight;
         for (let index = 0; index < canvasConfig.count; ++index) {
             points.push({
-                x: Math.random() * canvasRef.clientWidth,
-                y: Math.random() * canvasRef.clientHeight,
-                xStep: 2 * Math.random() - 1,       // step size of point movement 
+                x    : Math.random() * width,
+                y    : Math.random() * height,
+                xStep: 2 * Math.random() - 1,    // step size of point movement 
                 yStep: 2 * Math.random() - 1,
-                max: canvasConfig.dist
+                max  : canvasConfig.dist
             })
         }
         return points;
@@ -68,8 +74,8 @@ class ReactCanvasNest extends Component {
         const points = this.randomPoints();
 
         const mouseCoordinate = {    // mouse coordinate
-            x: null,
-            y: null,
+            x  : null,
+            y  : null,
             max: 20000
         };
 
@@ -85,21 +91,52 @@ class ReactCanvasNest extends Component {
     }
 
     drawNest = () => {
-        const { context, outDivWidth, outDivHeight, points, pointsWithMouse, canvasConfig, current } = this.state;
+        const { context, outDivWidth, outDivHeight, points, pointsWithMouse, canvasStyle, canvasConfig } = this.state;
+        const { pointColor, pointR, pointOpacity, lineWidth,lineColor }                                            = canvasConfig;
+
         context.clearRect(0, 0, outDivWidth, outDivHeight);
+
         for (let index = 0; index < points.length; ++index) {
             const point = points[index];
+            // context.fillRect(circle.x - 0.5, circle.y - 0.5, 1, 1);
             context.beginPath();
-            context.fillStyle = 'rgba(255,0,0,1)';
-            context.arc(point.x, point.y, 1, 0, 2 * Math.PI);
+            context.fillStyle = `rgba(${pointColor},${pointOpacity})`;
+            context.arc(point.x, point.y, pointR, 0, 2 * Math.PI);
             context.fill();
+
+            point.x     += point.xStep;                                                         // point movement
+            point.y     += point.yStep;
+            point.xStep *= (point.x + pointR > outDivWidth || point.x - pointR < 0) ? -1 : 1;
+            point.yStep *= (point.y + pointR > outDivHeight || point.y - pointR < 0) ? -1 : 1;
+
+            for (let nextIndex = 0; nextIndex < pointsWithMouse.length; ++nextIndex) {
+                const nextPoint = pointsWithMouse[nextIndex];
+
+                const xDist = point.x - nextPoint.x;
+                const yDist = point.y - nextPoint.y;
+                const dist  = xDist * xDist + yDist * yDist;  // the square of the distance between two points
+
+                if (dist < nextPoint.max && nextPoint.x !== null) {
+                    // dist < nextPoint.max && (
+                    //     nextPoint === current && dist >= nextPoint.max / 2 && (point.x -= 0.03 * x_dist, point.y -= 0.03 * y_dist));     // 靠近的时候加速
+                    const scale = (nextPoint.max - dist) / (nextPoint.max);
+                    // console.log(scale * lineWidth)
+                    context.beginPath();
+                    context.lineWidth   = (scale * lineWidth) / 2;
+                    context.strokeStyle = `rgba(${lineColor},${scale})`;
+                    context.moveTo(point.x, point.y);
+                    context.lineTo(nextPoint.x, nextPoint.y);
+                    context.stroke();
+                }
+            }
         }
+        requestAnimationFrame(this.drawNest);
     }
 
     render() {
         const { canvasStyle, outDivWidth, outDivHeight } = this.state;
         return (
-            <canvas ref={this.setCanvas} style={canvasStyle} width={outDivWidth} height={outDivHeight} className={this.props.className || ''} />
+            <canvas ref = {this.setCanvas} style = {canvasStyle} width = {outDivWidth} height = {outDivHeight} className = {this.props.className || ''} />
         )
     }
 }
